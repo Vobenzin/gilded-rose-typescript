38e3add30d7065f536d4fe282815395e4a86bac3
Vobenzin <adamecko2551@gmail.com>
2025-09-30 11:09:19 +0200

    AAApatern update tests

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -11,20 +11,38 @@ describe("Gilded Rose", () => {
         const items = regularItems.updateQuality();
 
         it("should degrades Quality twice as fast,once the sell by date has passed", () => {
-            expect(items[0].quality).toEqual(8);
+            const abItem =  items[0]
+
+            const val = abItem.quality
+
+            expect(val).toEqual(8);
         });
 
         it("should stop the quality value to be lower than zero", () => {
-            expect(items[1].quality).toEqual(0);
+            const abItem =  items[1]
+
+            const val = abItem.quality
+
+            expect(val).toEqual(0);
         });
 
-        it("should degrades sellin value and quality value", () => {
+        it("should degrades sellin value", () => {
             const gildedRose = new Shop([new Item("reg item", 2, 10)]);
-            const items = gildedRose.updateQuality();
-            expect(items[0].sellIn).toEqual(1);
-            expect(items[0].quality).toEqual(9);
-        });
 
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].sellIn).toEqual(1);
+            
+        });
+        it("should degrades quality value", () => {
+            const gildedRose = new Shop([new Item("reg item", 2, 10)]);
+
+            const items = gildedRose.updateQuality();
+
+            expect(items[0].quality).toEqual(9);
+            
+        });
+        
     });
     describe("Aged Brie",() => {
         const agerBrieItem = new Shop([
@@ -34,11 +52,19 @@ describe("Gilded Rose", () => {
         const items = agerBrieItem.updateQuality();
 
         it("should increase Aged Brie's quality the older it gets", () => {
-            expect(items[0].quality).toEqual(11);
+            const abItem =  items[0]
+
+            const val = abItem.quality
+
+            expect(val).toEqual(11);
         });
 
         it("should not exceed the Quality of 50", () => {
-            expect(items[1].quality).toEqual(50);
+            const abItem =  items[1]
+
+            const val = abItem.quality
+
+            expect(val).toEqual(50);
         });
 
     });
@@ -50,11 +76,19 @@ describe("Gilded Rose", () => {
         const items = sulfurasItem.updateQuality();
 
         it("should not decrease Sulfuras's quality", () => {
-            expect(items[0].quality).toEqual(80);
+            const sulfItem =  items[0]
+
+            const val = sulfItem.quality
+
+            expect(val).toEqual(80);
         });
 
         it("should not decrease Sulfuras's sellIn", () => {
-            expect(items[0].sellIn).toEqual(3);
+            const sulfItem =  items[0]
+
+            const val = sulfItem.sellIn
+
+            expect(val).toEqual(3);
         });
 
     });
@@ -69,19 +103,35 @@ describe("Gilded Rose", () => {
         const items = sulfurasItem.updateQuality();
 
         it("should increase quality when aging", () => {
-            expect(items[0].quality).toEqual(31);
+            const bpItem =  items[0]
+
+            const val = bpItem.quality
+
+            expect(val).toEqual(31);
         });
 
         it("should increase quality when aging by 2 when 10 or less days", () => {
-            expect(items[1].quality).toEqual(32);
+            const bpItem =  items[1]
+
+            const val = bpItem.quality
+
+            expect(val).toEqual(32);
         });
 
         it("should increase quality when aging by 3 when 5 or less days", () => {
-            expect(items[2].quality).toEqual(33);
+            const bpItem =  items[2]
+
+            const val = bpItem.quality
+
+            expect(val).toEqual(33);
         });
 
         it("should drop to 0 after the concert", () => {
-            expect(items[3].quality).toEqual(0);
+            const bpItem =  items[3]
+
+            const val = bpItem.quality
+
+            expect(val).toEqual(0);
         });
 
     });
@@ -94,15 +144,27 @@ describe("Gilded Rose", () => {
             ]);
             const items = conjuredItem.updateQuality();
         it("should drop quality by 2", () => {
-            expect(items[0].quality).toEqual(28);
+            const cngItem =  items[0]
+
+            const val = cngItem.quality
+
+            expect(val).toEqual(28);
         });
 
         it("should drop quality by 4", () => {
-            expect(items[1].quality).toEqual(26);
+            const cngItem =  items[1]
+
+            const val = cngItem.quality
+
+            expect(val).toEqual(26);
         });
 
         it("should not drop quality below 0", () => {
-            expect(items[2].quality).toEqual(0);
+            const cngItem =  items[2]
+
+            const val = cngItem.quality
+
+            expect(val).toEqual(0);
         });
     });
 });
